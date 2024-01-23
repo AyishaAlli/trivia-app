b564ab358e415812cdd5b52c4bad509c715fddb5
Ayisha Alli <alli.ayisha1@gmail.com>
2024-01-23 17:14:55 +0000

    added tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -56,7 +56,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(len(data["questions"]))
         self.assertTrue(len(data["categories"]))
 
-    def test_get_all_categories(self):
+    def test_get_categories(self):
         res = self.client().get('/categories')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
@@ -64,12 +64,50 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['categories'])
         self.assertTrue(data['total_categories'])
 
+    def test_delete_questions(self):
+        res = self.client().delete('questions/4')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], 11)
+    
+    def search_question(self):
+        res = self.client().post('questions/search', json={"searchTerm": "the"})
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['total_questions'])
+
     def test_400_get_quiz_without_body(self):
         res = self.client().post("/quizzes")
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 400)
         self.assertEqual(data["success"], False)
         self.assertEqual(data["message"], "Bad request")
+
+    def test_404_invalid_page_numbers(self):
+        res = self.client().get('/questions?page=200000')
+        data = json.loads(res.data)
+        self.assertEqual(data['error'], 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data["message"], "Resource not found")
+
+    def test_422_cannot_create_question(self):
+        new_question = { 
+        'question': 'What is the day after Tuesday?',
+        'category': '1',
+        'answer':'',
+        'difficulty': 1,
+        }
+
+        res = self.client().post('/questions', json=new_question)
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['error'], 422)
+        self.assertEqual(data['message'], "Unprocessable")
+
+        
+
 
     
 
